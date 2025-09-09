9952073f990655daae5b0d748575b169cc3d3f1b
Genius <genius@Mac-006.lan>
2025-09-09 11:26:28 +0200

    suspend sessions

--- back/migrations/Version20250909084040.php
+++ back/migrations/Version20250909084040.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250819131342 extends AbstractMigration
+final class Version20250909084040 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,17 @@ final class Version20250819131342 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE session DROP is_suspended');
+        $this->addSql('ALTER TABLE subscription ALTER is_suspended SET DEFAULT false');
+        $this->addSql('ALTER TABLE subscription ALTER is_suspended SET NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE Session ADD is_suspended BOOLEAN DEFAULT false NOT NULL');
+        $this->addSql('ALTER TABLE Subscription ALTER is_suspended DROP DEFAULT');
+        $this->addSql('ALTER TABLE Subscription ALTER is_suspended DROP NOT NULL');
     }
 }
